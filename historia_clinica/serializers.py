67b2a0aa810090bf52a1e18456a390112f831161
cjbm7 <afterone007@gmail.com>
2021-08-19 16:14:04 -0500

    delete serializers

--- historia_clinica/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Patient
-
-
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Patient
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
